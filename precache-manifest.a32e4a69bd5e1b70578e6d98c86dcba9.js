5db9b3f3bc956476c187fd4da61203e8a7bcc0e0
Germano Gascho <germanogascho@gmail.com>
2021-09-28 13:09:11 -0300

    Updates

---
+++ precache-manifest.a32e4a69bd5e1b70578e6d98c86dcba9.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5f826c66d3358132de786f87d298dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64a7c7fa80fbd71f068b",
+    "url": "./static/css/2.41f64e70.chunk.css"
+  },
+  {
+    "revision": "cfb4925681d5014a6b4b",
+    "url": "./static/css/main.a69b95ce.chunk.css"
+  },
+  {
+    "revision": "64a7c7fa80fbd71f068b",
+    "url": "./static/js/2.8252834b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8252834b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb4925681d5014a6b4b",
+    "url": "./static/js/main.c35f4132.chunk.js"
+  },
+  {
+    "revision": "436598b692f3c5211e09",
+    "url": "./static/js/runtime-main.25b60f89.js"
+  },
+  {
+    "revision": "06d6a788ef38be4434a7f1e324c5cf67",
+    "url": "./static/media/Beleren2016-Bold.06d6a788.ttf"
+  },
+  {
+    "revision": "6f4f54c77708eec29ffc6dec15916694",
+    "url": "./static/media/Beleren2016-Bold.6f4f54c7.svg"
+  },
+  {
+    "revision": "c2b0f65cfc0b6bb2653f2c40624dc224",
+    "url": "./static/media/Beleren2016-Bold.c2b0f65c.woff"
+  },
+  {
+    "revision": "1002a4560137ce51831f6de6e59161d7",
+    "url": "./static/media/Beleren2016SmallCaps-Bold.1002a456.woff"
+  },
+  {
+    "revision": "427549e957e2f652bcbfab1114a1f096",
+    "url": "./static/media/Beleren2016SmallCaps-Bold.427549e9.ttf"
+  },
+  {
+    "revision": "f9ba650a987c8812376f70a2b2fa67c9",
+    "url": "./static/media/Beleren2016SmallCaps-Bold.f9ba650a.svg"
+  },
+  {
+    "revision": "5645117e87e2b56309db58fc648cdc73",
+    "url": "./static/media/Beleren2016SmallCaps-BoldItalic.5645117e.ttf"
+  },
+  {
+    "revision": "df29f40092259f0bb5949308ddc83a98",
+    "url": "./static/media/Beleren2016SmallCaps-BoldItalic.df29f400.svg"
+  },
+  {
+    "revision": "fa927570c2d86e4fd648a7d50e2dfee4",
+    "url": "./static/media/Beleren2016SmallCaps-BoldItalic.fa927570.woff"
+  },
+  {
+    "revision": "3d3facbbeb313a5796f2b7b300d0883a",
+    "url": "./static/media/Gotham Medium Italic.3d3facbb.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "./static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "7ec0e17efa507c993e222fe72a058847",
+    "url": "./static/media/MPlantin-Bold.7ec0e17e.ttf"
+  },
+  {
+    "revision": "2985f9784f666fa9ea50a1beaef3fcd8",
+    "url": "./static/media/MPlantin-Italic.2985f978.ttf"
+  },
+  {
+    "revision": "806ea7dbaa3ad2a7683992e3ac80d691",
+    "url": "./static/media/MPlantin.806ea7db.ttf"
+  },
+  {
+    "revision": "5a5b181b7164f88b1eafeb11d3aef5b5",
+    "url": "./static/media/keyrune.5a5b181b.woff"
+  },
+  {
+    "revision": "6b1143f8c52ac60b155927baee56ff55",
+    "url": "./static/media/keyrune.6b1143f8.ttf"
+  },
+  {
+    "revision": "7f03aa35240f1a48efcdaacd244c9dad",
+    "url": "./static/media/keyrune.7f03aa35.eot"
+  },
+  {
+    "revision": "85c1e93c26d9109d32e076bebc1f4a1d",
+    "url": "./static/media/keyrune.85c1e93c.woff2"
+  },
+  {
+    "revision": "8ac3aec5893ce745bd79917b27b154d1",
+    "url": "./static/media/keyrune.8ac3aec5.svg"
+  },
+  {
+    "revision": "3490a96cd3e9cadec56a2f85a726ec25",
+    "url": "./static/media/mana.3490a96c.svg"
+  },
+  {
+    "revision": "5e1fd54783b0c12acfc7ce5e84f3d43a",
+    "url": "./static/media/mana.5e1fd547.ttf"
+  },
+  {
+    "revision": "8f579063a56d8214d6ea305e5540fad8",
+    "url": "./static/media/mana.8f579063.woff"
+  },
+  {
+    "revision": "d8900e7ba4598e8363608258795efe94",
+    "url": "./static/media/mana.d8900e7b.eot"
+  },
+  {
+    "revision": "7f51b3545752eb40bb4b63661f584d7d",
+    "url": "./static/media/mplantin.7f51b354.svg"
+  },
+  {
+    "revision": "b09923e4e9b689572c4d30429491b892",
+    "url": "./static/media/mplantin.b09923e4.woff"
+  },
+  {
+    "revision": "c64fa9e74b60a46076fce0b1a4b631dc",
+    "url": "./static/media/mplantin.c64fa9e7.ttf"
+  },
+  {
+    "revision": "daf8b76ba2ad29eac149b6a26a5747d7",
+    "url": "./static/media/mplantin.daf8b76b.eot"
+  }
+]);
